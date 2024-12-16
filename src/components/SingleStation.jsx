ba4cb5cdb40d4d580cfa8bae2985f27314ba2be7
cipher <dilsurbet@hmail.com>
2024-12-16 17:27:22 +0500

    123

--- src/components/SingleStation.jsx
+++ src/components/SingleStation.jsx
@@ -1,21 +1,11 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { db } from "../firebase/firebaseConfig";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 import path from "path";
-
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
-  plugins: [require("daisyui"), require("tailwindcss-animate")],
+  plugins: [daisyui, require("tailwindcss-animate")],
   theme: {
     extend: {
       borderRadius: {
